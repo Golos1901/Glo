6f26fbb24fe735d753f8a81aad45e44fea50d9c8
Golos1901 <adanos2007-cheh68@ya.ru>
2021-10-05 15:45:06 +0300

    Испарвил, дополнил

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Учу JS tefgsdfdsf xzvxzstddd!");
+let title = "Учу Уроки";
+let screens = 2345;
+let screenPrice=2589;
+let rollback = "js";
+let fullPrice = 9082;
+let adaptive = 3489;
+console.log(rollback);
+alert(title);
